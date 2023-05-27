a4f6b0516b6f5a074b24babefd786c95633653d7
Konrad Trusiuk <konrad.zahon@gmail.com>
2023-05-27 08:44:18 +0200

    feat: add story that contain colors using in project

---
+++ styles/colors.stories.tsx
@@ -0,0 +1,281 @@
+const meta = {
+  title: 'Styles/Colors',
+};
+
+export default meta;
+
+export const Colors = {
+  render: () => (
+    <div className="container mx-auto flex flex-col items-center gap-y-8 px-4">
+      <div className="flex flex-col gap-y-2">
+        <h2 className="text-lg font-bold">Neutral</h2>
+        <div className="flex flex-wrap gap-4">
+          <div className="flex flex-col items-center gap-y-1">
+            <div className="h-24 w-24 rounded-lg bg-neutral-1" />
+            <p className="text-sm">neutral-1</p>
+          </div>
+          <div className="flex flex-col items-center gap-y-1">
+            <div className="h-24 w-24 rounded-lg bg-neutral-2" />
+            <p className="text-sm">neutral-2</p>
+          </div>
+          <div className="flex flex-col items-center gap-y-1">
+            <div className="h-24 w-24 rounded-lg bg-neutral-3" />
+            <p className="text-sm">neutral-3</p>
+          </div>
+          <div className="flex flex-col items-center gap-y-1">
+            <div className="h-24 w-24 rounded-lg bg-neutral-4" />
+            <p className="text-sm">neutral-4</p>
+          </div>
+          <div className="flex flex-col items-center gap-y-1">
+            <div className="h-24 w-24 rounded-lg bg-neutral-5" />
+            <p className="text-sm">neutral-5</p>
+          </div>
+          <div className="flex flex-col items-center gap-y-1">
+            <div className="h-24 w-24 rounded-lg bg-neutral-6" />
+            <p className="text-sm">neutral-6</p>
+          </div>
+          <div className="flex flex-col items-center gap-y-1">
+            <div className="h-24 w-24 rounded-lg bg-neutral-7" />
+            <p className="text-sm">neutral-7</p>
+          </div>
+          <div className="flex flex-col items-center gap-y-1">
+            <div className="h-24 w-24 rounded-lg bg-neutral-8" />
+            <p className="text-sm">neutral-8</p>
+          </div>
+          <div className="flex flex-col items-center gap-y-1">
+            <div className="h-24 w-24 rounded-lg bg-neutral-9" />
+            <p className="text-sm">neutral-9</p>
+          </div>
+          <div className="flex flex-col items-center gap-y-1">
+            <div className="h-24 w-24 rounded-lg bg-neutral-10" />
+            <p className="text-sm">neutral-10</p>
+          </div>
+          <div className="flex flex-col items-center gap-y-1">
+            <div className="h-24 w-24 rounded-lg bg-neutral-11" />
+            <p className="text-sm">neutral-11</p>
+          </div>
+          <div className="flex flex-col items-center gap-y-1">
+            <div className="h-24 w-24 rounded-lg bg-neutral-12" />
+            <p className="text-sm">neutral-12</p>
+          </div>
+        </div>
+      </div>
+
+      <div className="flex flex-col gap-y-2">
+        <h2 className="text-lg font-bold">Primary</h2>
+        <div className="flex flex-wrap gap-4">
+          <div className="flex flex-col items-center gap-y-1">
+            <div className="h-24 w-24 rounded-lg bg-primary-1" />
+            <p className="text-sm">primary-1</p>
+          </div>
+          <div className="flex flex-col items-center gap-y-1">
+            <div className="h-24 w-24 rounded-lg bg-primary-2" />
+            <p className="text-sm">primary-2</p>
+          </div>
+          <div className="flex flex-col items-center gap-y-1">
+            <div className="h-24 w-24 rounded-lg bg-primary-3" />
+            <p className="text-sm">primary-3</p>
+          </div>
+          <div className="flex flex-col items-center gap-y-1">
+            <div className="h-24 w-24 rounded-lg bg-primary-4" />
+            <p className="text-sm">primary-4</p>
+          </div>
+          <div className="flex flex-col items-center gap-y-1">
+            <div className="h-24 w-24 rounded-lg bg-primary-5" />
+            <p className="text-sm">primary-5</p>
+          </div>
+          <div className="flex flex-col items-center gap-y-1">
+            <div className="h-24 w-24 rounded-lg bg-primary-6" />
+            <p className="text-sm">primary-6</p>
+          </div>
+          <div className="flex flex-col items-center gap-y-1">
+            <div className="h-24 w-24 rounded-lg bg-primary-7" />
+            <p className="text-sm">primary-7</p>
+          </div>
+          <div className="flex flex-col items-center gap-y-1">
+            <div className="h-24 w-24 rounded-lg bg-primary-8" />
+            <p className="text-sm">primary-8</p>
+          </div>
+          <div className="flex flex-col items-center gap-y-1">
+            <div className="h-24 w-24 rounded-lg bg-primary-9" />
+            <p className="text-sm">primary-9</p>
+          </div>
+          <div className="flex flex-col items-center gap-y-1">
+            <div className="h-24 w-24 rounded-lg bg-primary-10" />
+            <p className="text-sm">primary-10</p>
+          </div>
+          <div className="flex flex-col items-center gap-y-1">
+            <div className="h-24 w-24 rounded-lg bg-primary-11" />
+            <p className="text-sm">primary-11</p>
+          </div>
+          <div className="flex flex-col items-center gap-y-1">
+            <div className="h-24 w-24 rounded-lg bg-primary-12" />
+            <p className="text-sm">primary-12</p>
+          </div>
+        </div>
+      </div>
+
+      <div className="flex flex-col gap-y-2">
+        <h2 className="text-lg font-bold">Success</h2>
+        <div className="flex flex-wrap gap-4">
+          <div className="flex flex-col items-center gap-y-1">
+            <div className="h-24 w-24 rounded-lg bg-success-1" />
+            <p className="text-sm">success-1</p>
+          </div>
+          <div className="flex flex-col items-center gap-y-1">
+            <div className="h-24 w-24 rounded-lg bg-success-2" />
+            <p className="text-sm">success-2</p>
+          </div>
+          <div className="flex flex-col items-center gap-y-1">
+            <div className="h-24 w-24 rounded-lg bg-success-3" />
+            <p className="text-sm">success-3</p>
+          </div>
+          <div className="flex flex-col items-center gap-y-1">
+            <div className="h-24 w-24 rounded-lg bg-success-4" />
+            <p className="text-sm">success-4</p>
+          </div>
+          <div className="flex flex-col items-center gap-y-1">
+            <div className="h-24 w-24 rounded-lg bg-success-5" />
+            <p className="text-sm">success-5</p>
+          </div>
+          <div className="flex flex-col items-center gap-y-1">
+            <div className="h-24 w-24 rounded-lg bg-success-6" />
+            <p className="text-sm">success-6</p>
+          </div>
+          <div className="flex flex-col items-center gap-y-1">
+            <div className="h-24 w-24 rounded-lg bg-success-7" />
+            <p className="text-sm">success-7</p>
+          </div>
+          <div className="flex flex-col items-center gap-y-1">
+            <div className="h-24 w-24 rounded-lg bg-success-8" />
+            <p className="text-sm">success-8</p>
+          </div>
+          <div className="flex flex-col items-center gap-y-1">
+            <div className="h-24 w-24 rounded-lg bg-success-9" />
+            <p className="text-sm">success-9</p>
+          </div>
+          <div className="flex flex-col items-center gap-y-1">
+            <div className="h-24 w-24 rounded-lg bg-success-10" />
+            <p className="text-sm">success-10</p>
+          </div>
+          <div className="flex flex-col items-center gap-y-1">
+            <div className="h-24 w-24 rounded-lg bg-success-11" />
+            <p className="text-sm">success-11</p>
+          </div>
+          <div className="flex flex-col items-center gap-y-1">
+            <div className="h-24 w-24 rounded-lg bg-success-12" />
+            <p className="text-sm">success-12</p>
+          </div>
+        </div>
+      </div>
+
+      <div className="flex flex-col gap-y-2">
+        <h2 className="text-lg font-bold">Warning</h2>
+        <div className="flex flex-wrap gap-4">
+          <div className="flex flex-col items-center gap-y-1">
+            <div className="h-24 w-24 rounded-lg bg-warning-1" />
+            <p className="text-sm">warning-1</p>
+          </div>
+          <div className="flex flex-col items-center gap-y-1">
+            <div className="h-24 w-24 rounded-lg bg-warning-2" />
+            <p className="text-sm">warning-2</p>
+          </div>
+          <div className="flex flex-col items-center gap-y-1">
+            <div className="h-24 w-24 rounded-lg bg-warning-3" />
+            <p className="text-sm">warning-3</p>
+          </div>
+          <div className="flex flex-col items-center gap-y-1">
+            <div className="h-24 w-24 rounded-lg bg-warning-4" />
+            <p className="text-sm">warning-4</p>
+          </div>
+          <div className="flex flex-col items-center gap-y-1">
+            <div className="h-24 w-24 rounded-lg bg-warning-5" />
+            <p className="text-sm">warning-5</p>
+          </div>
+          <div className="flex flex-col items-center gap-y-1">
+            <div className="h-24 w-24 rounded-lg bg-warning-6" />
+            <p className="text-sm">warning-6</p>
+          </div>
+          <div className="flex flex-col items-center gap-y-1">
+            <div className="h-24 w-24 rounded-lg bg-warning-7" />
+            <p className="text-sm">warning-7</p>
+          </div>
+          <div className="flex flex-col items-center gap-y-1">
+            <div className="h-24 w-24 rounded-lg bg-warning-8" />
+            <p className="text-sm">warning-8</p>
+          </div>
+          <div className="flex flex-col items-center gap-y-1">
+            <div className="h-24 w-24 rounded-lg bg-warning-9" />
+            <p className="text-sm">warning-9</p>
+          </div>
+          <div className="flex flex-col items-center gap-y-1">
+            <div className="h-24 w-24 rounded-lg bg-warning-10" />
+            <p className="text-sm">warning-10</p>
+          </div>
+          <div className="flex flex-col items-center gap-y-1">
+            <div className="h-24 w-24 rounded-lg bg-warning-11" />
+            <p className="text-sm">warning-11</p>
+          </div>
+          <div className="flex flex-col items-center gap-y-1">
+            <div className="h-24 w-24 rounded-lg bg-warning-12" />
+            <p className="text-sm">warning-12</p>
+          </div>
+        </div>
+      </div>
+
+      <div className="flex flex-col gap-y-2">
+        <h2 className="text-lg font-bold">Error</h2>
+        <div className="flex flex-wrap gap-4">
+          <div className="flex flex-col items-center gap-y-1">
+            <div className="h-24 w-24 rounded-lg bg-error-1" />
+            <p className="text-sm">error-1</p>
+          </div>
+          <div className="flex flex-col items-center gap-y-1">
+            <div className="h-24 w-24 rounded-lg bg-error-2" />
+            <p className="text-sm">error-2</p>
+          </div>
+          <div className="flex flex-col items-center gap-y-1">
+            <div className="h-24 w-24 rounded-lg bg-error-3" />
+            <p className="text-sm">error-3</p>
+          </div>
+          <div className="flex flex-col items-center gap-y-1">
+            <div className="h-24 w-24 rounded-lg bg-error-4" />
+            <p className="text-sm">error-4</p>
+          </div>
+          <div className="flex flex-col items-center gap-y-1">
+            <div className="h-24 w-24 rounded-lg bg-error-5" />
+            <p className="text-sm">error-5</p>
+          </div>
+          <div className="flex flex-col items-center gap-y-1">
+            <div className="h-24 w-24 rounded-lg bg-error-6" />
+            <p className="text-sm">error-6</p>
+          </div>
+          <div className="flex flex-col items-center gap-y-1">
+            <div className="h-24 w-24 rounded-lg bg-error-7" />
+            <p className="text-sm">error-7</p>
+          </div>
+          <div className="flex flex-col items-center gap-y-1">
+            <div className="h-24 w-24 rounded-lg bg-error-8" />
+            <p className="text-sm">error-8</p>
+          </div>
+          <div className="flex flex-col items-center gap-y-1">
+            <div className="h-24 w-24 rounded-lg bg-error-9" />
+            <p className="text-sm">error-9</p>
+          </div>
+          <div className="flex flex-col items-center gap-y-1">
+            <div className="h-24 w-24 rounded-lg bg-error-10" />
+            <p className="text-sm">error-10</p>
+          </div>
+          <div className="flex flex-col items-center gap-y-1">
+            <div className="h-24 w-24 rounded-lg bg-error-11" />
+            <p className="text-sm">error-11</p>
+          </div>
+          <div className="flex flex-col items-center gap-y-1">
+            <div className="h-24 w-24 rounded-lg bg-error-12" />
+            <p className="text-sm">error-12</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  ),
+};
