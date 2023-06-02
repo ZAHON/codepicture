92a4292a338383920c71a0918c0c7fae11a82ce1
Konrad Trusiuk <konrad.zahon@gmail.com>
2023-06-02 10:17:42 +0200

    feat: add disableTransitionOnChange property to ThemeProvider

--- app/providers.tsx
+++ app/providers.tsx
@@ -5,5 +5,9 @@ import { ThemeProvider } from 'next-themes';
 export function Providers(props: { children: ReactNode }) {
   const { children } = props;
 
-  return <ThemeProvider attribute="class">{children}</ThemeProvider>;
+  return (
+    <ThemeProvider attribute="class" enableColorScheme={false} disableTransitionOnChange>
+      {children}
+    </ThemeProvider>
+  );
 }
