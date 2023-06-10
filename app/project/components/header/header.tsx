15391039e43dd4f6e1bcaead0d2eb7237f946e3d
Konrad Trusiuk <konrad.zahon@gmail.com>
2023-06-11 01:27:05 +0200

    feat: remove ThemeChanger component from project page header

--- app/project/components/header/header.tsx
+++ app/project/components/header/header.tsx
@@ -1,13 +1,9 @@
-import { ThemeChanger } from '@/components';
 import { DropdownMenu } from '..';
 
 export function Header() {
   return (
     <header className="flex h-14 items-center justify-between border-b border-b-neutral-5 bg-white px-4 dark:bg-black">
       <DropdownMenu />
-      <div className="flex items-center gap-x-2">
-        <ThemeChanger />
-      </div>
     </header>
   );
 }
