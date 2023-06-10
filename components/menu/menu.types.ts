58aaa2846ceda1baaa4fdea3a5ff4071be6c84d6
Konrad Trusiuk <konrad.zahon@gmail.com>
2023-06-11 01:19:24 +0200

    feat: add onOpenChange property for Menu component

--- components/menu/menu.types.ts
+++ components/menu/menu.types.ts
@@ -17,6 +17,11 @@ export interface MenuProps {
   open?: boolean;
 
   /**
+   * Event handler called when the open state of the dropdown menu changes.
+   */
+  onOpenChange?: (open: boolean) => void;
+
+  /**
    * The modality of the menu. When set to true, interaction with outside elements will be disabled and only menu content will be visible to screen readers.
    * @default true
    */
