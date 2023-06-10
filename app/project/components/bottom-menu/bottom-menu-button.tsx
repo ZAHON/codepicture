1be25075da6cf03e103b374e6f43d01d8db59e5f
Konrad Trusiuk <konrad.zahon@gmail.com>
2023-06-10 17:19:56 +0200

    feat: add aria-label property for BottomMenuButton component

--- app/project/components/bottom-menu/bottom-menu-button.tsx
+++ app/project/components/bottom-menu/bottom-menu-button.tsx
@@ -8,11 +8,12 @@ interface BottomMenuButtonProps {
 }
 
 export function BottomMenuButton(props: BottomMenuButtonProps) {
-  const { onClick, children } = props;
+  const { onClick, ariaLabel, children } = props;
 
   return (
     <button
       onClick={onClick}
+      aria-label={ariaLabel}
       className="flex flex-1 select-none items-center justify-center focus-visible:bg-neutral-5 enabled:hover:bg-neutral-5 enabled:active:scale-95 enabled:active:bg-neutral-6 motion-safe:transition motion-safe:duration-150"
     >
       {children}
