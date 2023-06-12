f7e3bb3c119aea1499611314b24372b79325a1b9
Konrad Trusiuk <konrad.zahon@gmail.com>
2023-06-13 01:05:58 +0200

    feat: add new themes
    - Ayu Dark
    - Ayu Light
    - Ayu Mirage
    - Catppuccin Frappé
    - Catppuccin Latte
    - Catppuccin Macchiato
    - Catppuccin Mocha
    - Cobalt2
    - Darcula
    - Dracula
    - Monokai Dimmend
    - Monokai
    - Night Owl Light
    - Night Owl
    - Vitesse Black
    - Vitesse Dark Soft
    - Vitesse Dark
    - Vitesse Light Soft
    - Vitesse Light

--- data/themes/themes.ts
+++ data/themes/themes.ts
@@ -7,8 +7,58 @@ export const themes: Theme[] = [
     type: 'light',
   },
   {
+    id: 'ayu-dark',
+    label: 'Ayu Dark',
+    type: 'dark',
+  },
+  {
+    id: 'ayu-light',
+    label: 'Ayu Light',
+    type: 'light',
+  },
+  {
+    id: 'ayu-mirage',
+    label: 'Ayu Mirage',
+    type: 'dark',
+  },
+  {
+    id: 'catppuccin-frappe',
+    label: 'Catppuccin Frappé',
+    type: 'dark',
+  },
+  {
+    id: 'catppuccin-latte',
+    label: 'Catppuccin Latte',
+    type: 'light',
+  },
+  {
+    id: 'catppuccin-macchiato',
+    label: 'Catppuccin Macchiato',
+    type: 'dark',
+  },
+  {
+    id: 'catppuccin-mocha',
+    label: 'Catppuccin Mocha',
+    type: 'dark',
+  },
+  {
+    id: 'cobalt2',
+    label: 'Cobalt2',
+    type: 'dark',
+  },
+  {
+    id: 'darcula',
+    label: 'Darcula',
+    type: 'dark',
+  },
+  {
     id: 'dark-modern',
     label: 'Dark Modern',
+    type: 'dark',
+  },
+  {
+    id: 'dracula',
+    label: 'Dracula',
     type: 'dark',
   },
   {
@@ -52,6 +102,26 @@ export const themes: Theme[] = [
     type: 'light',
   },
   {
+    id: 'monokai-dimmend',
+    label: 'Monokai Dimmend',
+    type: 'dark',
+  },
+  {
+    id: 'monokai',
+    label: 'Monokai',
+    type: 'dark',
+  },
+  {
+    id: 'night-owl-light',
+    label: 'Night Owl Light',
+    type: 'light',
+  },
+  {
+    id: 'night-owl',
+    label: 'Night Owl',
+    type: 'dark',
+  },
+  {
     id: 'one-dark-pro-darker',
     label: 'One Dark Pro Darker',
     type: 'dark',
@@ -80,5 +150,30 @@ export const themes: Theme[] = [
     id: 'tokyo-night',
     label: 'Tokyo Night',
     type: 'dark',
+  },
+  {
+    id: 'vitesse-black',
+    label: 'Vitesse Black',
+    type: 'dark',
+  },
+  {
+    id: 'vitesse-dark-soft',
+    label: 'Vitesse Dark Soft',
+    type: 'dark',
+  },
+  {
+    id: 'vitesse-dark',
+    label: 'Vitesse Dark',
+    type: 'dark',
+  },
+  {
+    id: 'vitesse-light-soft',
+    label: 'Vitesse Light Soft',
+    type: 'light',
+  },
+  {
+    id: 'vitesse-light',
+    label: 'Vitesse Light',
+    type: 'light',
   },
 ];
--- data/themes/themes.types.ts
+++ data/themes/themes.types.ts
@@ -2,7 +2,17 @@ import type { CSSProperties } from 'react';
 
 export type ThemeId =
   | 'atom-one-light'
+  | 'ayu-dark'
+  | 'ayu-light'
+  | 'ayu-mirage'
+  | 'catppuccin-frappe'
+  | 'catppuccin-latte'
+  | 'catppuccin-macchiato'
+  | 'catppuccin-mocha'
+  | 'cobalt2'
+  | 'darcula'
   | 'dark-modern'
+  | 'dracula'
   | 'github-dark-colorblind'
   | 'github-dark-default'
   | 'github-dark-dimmend'
@@ -11,12 +21,21 @@ export type ThemeId =
   | 'github-light-colorblind'
   | 'github-light'
   | 'light-modern'
+  | 'monokai-dimmend'
+  | 'monokai'
+  | 'night-owl-light'
+  | 'night-owl'
   | 'one-dark-pro-darker'
   | 'one-dark-pro-mix'
   | 'one-dark-pro'
   | 'tokyo-night-light'
   | 'tokyo-night-storm'
-  | 'tokyo-night';
+  | 'tokyo-night'
+  | 'vitesse-black'
+  | 'vitesse-dark-soft'
+  | 'vitesse-dark'
+  | 'vitesse-light-soft'
+  | 'vitesse-light';
 
 export type Theme = {
   id: ThemeId;
