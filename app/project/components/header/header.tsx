ab9b929e56404f1ee2cc33f775d6fb8f134b8ab8
Konrad Trusiuk <konrad.zahon@gmail.com>
2023-06-14 00:04:03 +0200

    feat: add logo into project page header

--- app/project/components/header/header.tsx
+++ app/project/components/header/header.tsx
@@ -1,8 +1,10 @@
+import { Logo } from '@/svgs';
 import { DropdownMenu } from '..';
 
 export function Header() {
   return (
-    <header className="flex h-14 items-center justify-between border-b border-b-neutral-5 bg-white px-4 dark:bg-black">
+    <header className="flex h-14  items-center justify-between border-b border-b-neutral-5 bg-white px-4 dark:bg-black">
+      <Logo height="1.5rem" />
       <DropdownMenu />
     </header>
   );
