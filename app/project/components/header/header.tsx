d002b6611c8ceb34765fcfab8b99c5d5d320a939
Konrad Trusiuk <konrad.zahon@gmail.com>
2023-06-18 20:42:26 +0200

    feat: add UserMenu component into project page header

--- app/project/components/header/header.tsx
+++ app/project/components/header/header.tsx
@@ -1,11 +1,16 @@
 import { Logo } from '@/svgs';
+import { UserMenu } from '@/components';
 import { DropdownMenu } from '..';
 
 export function Header() {
   return (
     <header className="flex h-14  items-center justify-between border-b border-b-neutral-5 bg-white px-4 dark:bg-black">
       <Logo height="1.5rem" />
-      <DropdownMenu />
+      <div className="flex items-center justify-center gap-x-4">
+        <DropdownMenu />
+        {/* @ts-expect-error Server Component */}
+        <UserMenu />
+      </div>
     </header>
   );
 }
