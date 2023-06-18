5df37e996d24716608f9f5184860280723fca1e0
Konrad Trusiuk <konrad.zahon@gmail.com>
2023-06-18 20:40:41 +0200

    feat: add SessionProvider into providers.tsx

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,13 +1,16 @@
 'use client';
 import type { ReactNode } from 'react';
 import { ThemeProvider } from 'next-themes';
+import { SessionProvider } from 'next-auth/react';
 
 export function Providers(props: { children: ReactNode }) {
   const { children } = props;
 
   return (
-    <ThemeProvider attribute="class" enableColorScheme={false} disableTransitionOnChange>
-      {children}
-    </ThemeProvider>
+    <SessionProvider>
+      <ThemeProvider attribute="class" enableColorScheme={false} disableTransitionOnChange>
+        {children}
+      </ThemeProvider>
+    </SessionProvider>
   );
 }
