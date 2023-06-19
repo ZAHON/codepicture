4b4b9d04d6ca2696a55d98d18a117c9593b42ed8
Konrad Trusiuk <konrad.zahon@gmail.com>
2023-06-20 00:10:00 +0200

    feat: add redirect to home page from projects page when user is not login

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,4 +1,14 @@
-export default function ProjectsPage() {
+import { redirect } from 'next/navigation';
+import { getServerSession } from 'next-auth/next';
+import { authOptions } from '@/lib/auth';
+
+export default async function ProjectsPage() {
+  const session = await getServerSession(authOptions);
+
+  if (!session) {
+    redirect('/');
+  }
+
   return (
     <div className="p-4">
       <h1>Projects page</h1>
