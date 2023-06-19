ebc59f7efb070ed13b6a5b6b0063409294f7a842
Konrad Trusiuk <konrad.zahon@gmail.com>
2023-06-19 19:28:03 +0200

    feat: add Header component into projects page

--- app/projects/layout.tsx
+++ app/projects/layout.tsx
@@ -1,7 +1,13 @@
 import type { ReactNode } from 'react';
+import { Header } from './components';
 
 export default function ProjectsPageLayout(props: { children: ReactNode }) {
   const { children } = props;
 
-  return <div>{children}</div>;
+  return (
+    <>
+      <Header />
+      <main>{children}</main>
+    </>
+  );
 }
--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,11 +1,7 @@
-import { UserMenu } from '@/components';
-
 export default function ProjectsPage() {
   return (
     <div className="p-4">
       <h1>Projects page</h1>
-      {/* @ts-expect-error Server Component */}
-      <UserMenu />
     </div>
   );
 }
