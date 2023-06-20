386aa9bf6f3412caa7fc76db59bffdb57e755675
Konrad Trusiuk <konrad.zahon@gmail.com>
2023-06-20 22:00:50 +0200

    feat: update functionality that allow create new project

--- app/projects/components/create-project/create-project.tsx
+++ app/projects/components/create-project/create-project.tsx
@@ -3,24 +3,60 @@ import { useTransition } from 'react';
 import { IconPlus } from '@tabler/icons-react';
 import { useSession } from 'next-auth/react';
 import { Button } from '@/components';
+import { notify } from '@/lib';
 import { createProject } from '../../server-actions';
 
 export function CreateProject() {
   const { data: session } = useSession();
   const [isPending, startTransition] = useTransition();
 
+  const ownerId = session?.user?.id;
+
   function handleCreateProject() {
-    const ownerId = session?.user?.id;
+    const notificationId = crypto.randomUUID();
 
     if (ownerId) {
-      startTransition(() => createProject(ownerId));
+      startTransition(async () => {
+        try {
+          notify.show({
+            id: notificationId,
+            position: 'bottom-right',
+            type: 'loading',
+            message: `New project is being created`,
+          });
+
+          await createProject(ownerId);
+
+          notify.show({
+            id: notificationId,
+            position: 'bottom-right',
+            type: 'success',
+            message: `New project has been created`,
+          });
+        } catch {
+          notify.show({
+            id: notificationId,
+            position: 'bottom-right',
+            type: 'error',
+            message: 'An error occurred while creating a new project',
+          });
+        }
+      });
+    } else {
+      notify.show({
+        id: notificationId,
+        position: 'bottom-right',
+        type: 'error',
+        message: 'An error occurred while creating a new project',
+      });
     }
   }
 
   return (
     <Button
       onClick={handleCreateProject}
-      disabled={isPending}
+      loading={isPending}
+      disabled={!Boolean(ownerId)}
       icon={<IconPlus size="1.25rem" aria-hidden="true" focusable="false" />}
     >
       New
