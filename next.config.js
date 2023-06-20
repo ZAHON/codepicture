fb80f80021de711552404fef00783b64791b481e
Konrad Trusiuk <konrad.zahon@gmail.com>
2023-06-20 06:03:51 +0200

    chore: update next.config.js

--- next.config.js
+++ next.config.js
@@ -10,7 +10,7 @@ const nextConfig = {
   },
   experimental: {
     serverActions: true,
-    serverComponentsExternalPackages: ['@prisma/client'],
+    serverComponentsExternalPackages: ['@prisma/client', 'prisma'],
   },
 };
 
