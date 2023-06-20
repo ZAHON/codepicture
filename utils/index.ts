36d181c77ae3b562ce772c15bc506ec57d7bd5a5
Konrad Trusiuk <konrad.zahon@gmail.com>
2023-06-20 06:01:52 +0200

    feat: add formatDistanceToNow utility function

--- utils/index.ts
+++ utils/index.ts
@@ -1,4 +1,5 @@
 export { applayComponentDefaultProps } from './applay-component-default-props';
+export { formatDistanceToNow } from './format-distance-to-now';
 export { getFillValueById } from './get-fill-value-by-id';
 export { getFontFamilyCssVariableById } from './get-font-family-css-variable-by-id';
 export { getShadowValueById } from './get-shadow-value-by-id';
