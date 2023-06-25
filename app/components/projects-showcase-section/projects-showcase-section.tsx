74687da2c78771fe742f3d971aa1bf5282f7f7ea
Konrad Trusiuk <konrad.zahon@gmail.com>
2023-06-25 20:50:14 +0200

    feat: update projects showcase section on home page

--- app/components/projects-showcase-section/projects-showcase-section.tsx
+++ app/components/projects-showcase-section/projects-showcase-section.tsx
@@ -3,7 +3,7 @@ import { LinkAsButton } from '@/components';
 export function ProjectsShowcaseSection() {
   return (
     <section className="my-8 flex flex-col items-center justify-center gap-8 rounded-lg border border-neutral-5 bg-neutral-1 p-8 dark:bg-neutral-2 md:flex-row">
-      <div className="flex flex-col items-center justify-center gap-y-8">
+      <div className="flex flex-col items-center justify-center gap-y-8 md:flex-1">
         <h2 className="text-center text-5xl font-bold">Store your snippets</h2>
         <p className="text-center text-lg">
           You want to keep track of your snippets, and have a safe place to save them.
@@ -15,7 +15,7 @@ export function ProjectsShowcaseSection() {
           *You must be authenticated to save your snippets remotely.
         </p>
       </div>
-      <div className="aspect-square w-full max-w-[27.75rem] bg-[url('/images/projects-showcase-light.png')] bg-contain bg-center bg-no-repeat drop-shadow-lg dark:bg-[url('/images/projects-showcase-dark.png')]" />
+      <div className="aspect-square w-full max-w-[27.75rem] overflow-hidden rounded-lg bg-[url('/images/projects-showcase-light.png')] bg-cover bg-left-top bg-no-repeat drop-shadow-lg dark:bg-[url('/images/projects-showcase-dark.png')] md:flex-1" />
     </section>
   );
 }
