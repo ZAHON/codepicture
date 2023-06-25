8f60b25377c56e7358fcefaa73c5a1650d1e1743
Konrad Trusiuk <konrad.zahon@gmail.com>
2023-06-25 08:50:44 +0200

    feat: add redirect to projects page when logged user visit project page

--- app/project/page.tsx
+++ app/project/page.tsx
@@ -1,6 +1,15 @@
+import { redirect } from 'next/navigation';
+import { getServerSession } from 'next-auth/next';
+import { authOptions } from '@/lib-server/auth';
 import { LoadingOverlay, Sandbox } from './components';
 
-export default function ProjectPage() {
+export default async function ProjectPage() {
+  const session = await getServerSession(authOptions);
+
+  if (session) {
+    redirect('/projects');
+  }
+
   return (
     <>
       <LoadingOverlay />
