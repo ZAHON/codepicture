9ae085286479c1e3871aed8dfd2f567b426571e9
Konrad Trusiuk <konrad.zahon@gmail.com>
2023-06-30 11:15:43 +0200

    feat: change span element that contains credentials into p element in home page footer

--- app/components/footer/footer.tsx
+++ app/components/footer/footer.tsx
@@ -4,7 +4,7 @@ export function Footer() {
   return (
     <div className="border-t border-t-neutral-5">
       <footer className="flex h-14 items-center justify-between px-4 lg:mx-auto lg:max-w-7xl">
-        <span className="text-sm font-medium">© 2023 Konrad Trusiuk</span>
+        <p className="text-sm font-medium">© 2023 Konrad Trusiuk</p>
         <ThemeChanger />
       </footer>
     </div>
